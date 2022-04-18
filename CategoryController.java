f75372ade8ede2444347aa4f9160c5465201934b
suchit98 <41755095+suchit98@users.noreply.github.com>
2022-04-18 18:25:28 +0530

    Pagination added

---
+++ CategoryController.java
@@ -0,0 +1,85 @@
+package com.example.shop.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.shop.exception.CategoryException;
+import com.example.shop.model.Category;
+import com.example.shop.repository.CategoryRepository;
+
+@RestController
+@RequestMapping(value = "/api")
+public class CategoryController {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	@PostMapping("/categories")
+	public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+		Category categoryAdded = categoryRepository.save(category);
+		if (categoryAdded == null) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<>(categoryAdded, HttpStatus.CREATED);
+	}
+
+	@GetMapping("/categories/{di}")
+	public ResponseEntity<Category> getCategoryById(@PathVariable(value = "di") Integer id) {
+		Category category = categoryRepository.findById(id)
+				.orElseThrow(() -> new CategoryException("Not found Category with id = " + id));
+		return new ResponseEntity<>(category, HttpStatus.OK);
+	}
+
+	@PutMapping("/categories/{di}")
+	public ResponseEntity<Category> updateCategory(@PathVariable("di") Integer id,
+			@RequestBody Category categoryRequest) {
+		Category category = categoryRepository.findById(id)
+				.orElseThrow(() -> new CategoryException("CategoryId " + id + "not found"));
+		category.setCategoryName(categoryRequest.getCategoryName());
+		return new ResponseEntity<>(categoryRepository.save(category), HttpStatus.OK);
+	}
+
+	@DeleteMapping("/categories/{di}")
+	public ResponseEntity<HttpStatus> deleteCategory(@PathVariable("di") Integer id) {
+		categoryRepository.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+	@GetMapping("/categories")
+	public ResponseEntity<List<Category>> getAllCategory() {
+
+		List<Category> category = categoryRepository.findAll();
+		return new ResponseEntity<>(category, HttpStatus.OK);
+	}
+	
+	@GetMapping("/categories")
+	public List<Category> getAllCatgoryLimit(@RequestParam("page") Integer pageNo)
+    {
+        Pageable paging = PageRequest.of(pageNo, 5);
+ 
+        Page<Category> pagedResult = categoryRepository.findAll(paging);
+         
+        if(pagedResult.hasContent()) {
+            return pagedResult.getContent();
+        } else {
+            return new ArrayList<Category>();
+        }
+    }
+
+}
---
+++ ProductController.java
@@ -0,0 +1,92 @@
+package com.example.shop.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.shop.exception.ProductException;
+import com.example.shop.model.Product;
+import com.example.shop.repository.ProductRepository;
+
+@RestController
+@RequestMapping(value = "/api")
+public class ProductController {
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	@GetMapping("/products/")
+	public ResponseEntity<List<Product>> getAllProducts() {
+		
+		
+		List<Product> productList = productRepository.findAll();
+		if (productList.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<>(productList, HttpStatus.OK);
+	}
+
+	@GetMapping("/products/{di}")
+	public ResponseEntity<Product> getProductById(@PathVariable("di") long id) {
+		Product product = productRepository.findById((int) id)
+				.orElseThrow(() -> new ProductException("Not found Product with id = " + id));
+		return new ResponseEntity<>(product, HttpStatus.OK);
+	}
+
+	@PostMapping("/products")
+	public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+		Product productAdded = productRepository.save(product);
+		if (productAdded == null) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<>(productAdded, HttpStatus.CREATED);
+	}
+
+	@PutMapping("/products/{di}")
+	public ResponseEntity<Product> updateProduct(@PathVariable("di") long id, @RequestBody Product product) {
+		Product productFound = productRepository.findById((int) id)
+				.orElseThrow(() -> new ProductException("Not found Product with id = " + id));
+		productFound.setProductCount(product.getProductCount());
+		productFound.setProductdesc(product.getProductdesc());
+		productFound.setProductName(product.getProductName());
+		productFound.setProductPrice(product.getProductPrice());
+		productFound.setProductType(product.getProductType());
+
+		return new ResponseEntity<>(productRepository.save(productFound), HttpStatus.OK);
+	}
+
+	@DeleteMapping("/products/{di}")
+	public ResponseEntity<HttpStatus> deleteTutorial(@PathVariable("di") long id) {
+		productRepository.deleteById((int) id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+	
+	@GetMapping("/products")
+	public List<Product> getAllProductLimit(@RequestParam("page") Integer pageNo)
+    {
+        Pageable paging = PageRequest.of(pageNo, 5);
+ 
+        Page<Product> pagedResult = productRepository.findAll(paging);
+         
+        if(pagedResult.hasContent()) {
+            return pagedResult.getContent();
+        } else {
+            return new ArrayList<Product>();
+        }
+    }
+}
